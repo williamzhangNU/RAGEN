f7162a20f4b196998c341dc80a9a0c34f70de16a
tenacious <tenacioustommy6@gmail.com>
2025-08-26 22:40:24 +0800

    update

--- ragen/llm_agent/agent_proxy.py
+++ ragen/llm_agent/agent_proxy.py
@@ -12,7 +12,10 @@ from omegaconf import ListConfig, DictConfig, OmegaConf
 from verl.protocol import pad_dataproto_to_divisor, unpad_dataproto
 from .ctx_manager import ContextManager
 from .es_manager import EnvStateManager
-from vllm import LLM, SamplingParams
+try:
+	from vllm import LLM, SamplingParams
+except ImportError:
+	pass
 from verl.single_controller.ray.base import RayWorkerGroup
 
 from ragen.env.spatial.env import SpatialGym
